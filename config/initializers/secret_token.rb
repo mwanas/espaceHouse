d7bd46fb9cbdab16ae0ea0ecf47847adb7b6bf4c
mai wanas <mai.wanas@espace.com.eg>
2013-06-11 14:42:45 +0200

    my first commit on GitHub for espaceHouse

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EspaceHouse::Application.config.secret_token = '67de12fe388cc6838244402397acc5b5bc97b25a708a73cc52dcc89b5b7a0c0181f5b705ac3b988eb56ed4f3d8b9b2dfcd4dff5e4ab101c5b949fdbf27d70bc1'
